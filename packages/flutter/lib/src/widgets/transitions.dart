fa690d3d74eef07b43d0ebad25eeee08f2131708
Nate Wilson <nate.w5687@gmail.com>
2024-11-07 17:10:53 -0700

    update `MatrixTransition`

--- packages/flutter/lib/src/widgets/transitions.dart
+++ packages/flutter/lib/src/widgets/transitions.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: public_member_api_docs
+
 /// @docImport 'package:flutter/material.dart';
 /// @docImport 'package:flutter/widgets.dart';
 library;
@@ -279,7 +281,7 @@ typedef TransformCallback = Matrix4 Function(double animationValue);
 ///    matrix which scales along the X and Y axis.
 ///  * [RotationTransition], which animates the rotation of a widget, by
 ///    providing a matrix which rotates along the Z axis.
-class MatrixTransition extends SingleChildRenderObjectWidget with RenderListenable {
+class MatrixTransition extends SingleChildRenderObjectWidget {
   /// Creates a matrix transition.
   ///
   /// The [alignment] argument defaults to [Alignment.center].
@@ -292,18 +294,15 @@ class MatrixTransition extends SingleChildRenderObjectWidget with RenderListenab
     super.child,
   });
 
-  /// The callback to compute a [Matrix4] from the [animation]. It's called
-  /// every time [animation] changes its value.
-  final TransformCallback onTransform;
-
   /// The animation that controls the matrix of the child.
   ///
   /// The matrix will be computed from the animation with the [onTransform]
   /// callback.
   final Animation<double> animation;
 
-  @override
-  Listenable get listenable => animation;
+  /// The callback to compute a [Matrix4] from the [animation]. It's called
+  /// every time [animation] changes its value.
+  final TransformCallback onTransform;
 
   /// The alignment of the origin of the coordinate system in which the
   /// transform takes place, relative to the size of the box.
@@ -321,20 +320,81 @@ class MatrixTransition extends SingleChildRenderObjectWidget with RenderListenab
   final FilterQuality? filterQuality;
 
   @override
-  RenderObject createRenderObject(BuildContext context) {
-    return RenderTransform(
-      transform: onTransform(animation.value),
+  RenderTransform createRenderObject(BuildContext context) {
+    // The ImageFilter layer created by setting filterQuality will introduce
+    // a saveLayer call. This is usually worthwhile when animating the layer,
+    // but leaving it in the layer tree before the animation has started or after
+    // it has finished significantly hurts performance.
+    return RenderAnimatedTransform(
+      animation: animation,
+      onTransform: onTransform,
       alignment: alignment,
-      filterQuality: animation.isAnimating ? filterQuality : null,
+      filterQuality: filterQuality,
+      textDirection: Directionality.maybeOf(context),
     );
   }
 
   @override
   void updateRenderObject(BuildContext context, RenderTransform renderObject) {
-    renderObject
-      ..transform = onTransform(animation.value)
+    (renderObject as RenderAnimatedTransform)
+      ..animation = animation
+      ..onTransform = onTransform
       ..alignment = alignment
-      ..filterQuality = animation.isAnimating ? filterQuality : null;
+      ..widgetFilterQuality = filterQuality
+      ..textDirection = Directionality.maybeOf(context);
+  }
+}
+
+class RenderAnimatedTransform extends RenderTransform {
+  RenderAnimatedTransform({
+    required Animation<double> animation,
+    required this.onTransform,
+    super.alignment,
+    super.textDirection,
+    FilterQuality? filterQuality,
+    // super.origin,
+    // super.transformHitTests,
+  }) : _widgetFilterQuality = filterQuality,
+       _animation = animation,
+       super(
+         transform: onTransform(animation.value),
+         filterQuality: animation.isAnimating ? filterQuality : null,
+       ) {
+    animation.addListener(listener);
+  }
+
+  Animation<double> get animation => _animation;
+  Animation<double> _animation;
+  set animation(Animation<double> newValue) {
+    if (newValue == _animation) {
+      return;
+    }
+    _animation.removeListener(listener);
+    _animation = newValue..addListener(listener);
+    listener();
+  }
+
+  /// The [FilterQuality] as configured by [MatrixTransition.filterQuality].
+  ///
+  /// Applies an [ImageFilterLayer] only while the animation is in progress.
+  FilterQuality? get widgetFilterQuality => _widgetFilterQuality;
+  FilterQuality? _widgetFilterQuality;
+  set widgetFilterQuality(FilterQuality? newValue) {
+    _widgetFilterQuality = newValue;
+    filterQuality = animation.isAnimating ? newValue : null;
+  }
+
+  TransformCallback onTransform;
+
+  void listener() {
+    transform = onTransform(animation.value);
+    widgetFilterQuality = _widgetFilterQuality;
+  }
+
+  @override
+  void dispose() {
+    animation.removeListener(listener);
+    super.dispose();
   }
 }
 
